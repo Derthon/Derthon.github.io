600e711b31163b9f705e335224930319b8278244
Brian <briannunez1130@gmail.com>
2017-11-06 16:19:23 -0700

    Update: Hector's Page

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function openPage(name) {
 	var dir = "../" + name + "/" + name + ".html";
 	window.open(dir);
 }
+function hector(){
+	var down1 = document.getElementById('hector1');
+	down1.href;
+	var down2 = document.getElementById('hector1');
+	down2.href;
+}
